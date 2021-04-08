b5751653d071309a25d4258b9b25b14f0242c6f6
Andrea Panico <andrea@MBP-di-Andrea.homenet.telecomitalia.it>
2021-04-08 20:32:53 +0200

    aggiunta server

--- firstApp Teo.js
+++ firstApp Teo.js
@@ -1,4 +1,5 @@
 const sensorLib = require(`node-dht-sensor`); // include existing module called ‘node-dht-sensor
+const http = require('http')
 // Setup sensor, exit if failed
 var sensorType = 11; // 11 for DHT11, 22 for DHT22 and AM2302 var sensorPin = 4; // The GPIO pin number for sensor signal
 if (!sensorLib.initialize(sensorType, sensorPin))
@@ -12,4 +13,42 @@ setInterval (function(){
 
     console.log('Temperature:', readout.temperature.toFixed(1) + 'C');
     console.log('Humidity: ', readout.humidity.toFixed(1) + '%');
+    
+    var temperature = readout.temperature.toFixed(1);
+
+    const data = JSON.stringify({
+
+        "sensor":"ID1",
+        "timestamp": 123456789,
+        "temperature":20
+})
+      
+      const options = {
+        hostname: '192.168.1.1',
+        port: 3000,
+        path: '/temperature',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': data.length
+        }
+      }
+      
+      const req = http.request(options, res => {
+        console.log(`statusCode: ${res.statusCode}`)
+      
+        res.on('data', d => {
+          process.stdout.write(d)
+        })
+      })
+      
+      req.on('error', error => {
+        console.error(error)
+      })
+      
+      req.write(data)
+      req.end()
+
 }, 2000);
+
+
